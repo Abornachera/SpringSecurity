de4ae8e8e9320620e4df91f98b6f64e4cdcf007d
Anderson Bornachera <anderson@example.com>
2025-05-02 18:58:15 -0500

    First Commit

--- src/main/java/edu/unimagdalena/ssecurity/security/service/UserInfoDetail.java
+++ src/main/java/edu/unimagdalena/ssecurity/security/service/UserInfoDetail.java
@@ -1,7 +1,9 @@
 package edu.unimagdalena.ssecurity.security.service;
 
+import edu.unimagdalena.ssecurity.entities.User;
 import lombok.NoArgsConstructor;
 import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Service;
 
@@ -9,17 +11,25 @@ import java.util.Collection;
 import java.util.List;
 import java.util.Set;
 
-@Service
-@NoArgsConstructor
 public class UserInfoDetail  implements UserDetails {
     private final Long id;
     private final String username;
     private final String password;
+    private final String email;
     private final Set<GrantedAuthority> authorities;
+
+    public UserInfoDetail(User user){
+        this.id = user.getId();
+        this.username = user.getUsername();
+        this.password = user.getPassword();
+        this.email = user.getEmail();
+        this.authorities = user.getRoles().stream().
+                map(role -> new SimpleGrantedAuthority())
+    }
 
     @Override
     public Collection<? extends GrantedAuthority> getAuthorities() {
-        return List.of();
+        return null;
     }
 
     @Override
